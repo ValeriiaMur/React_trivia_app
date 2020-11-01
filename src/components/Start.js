e5f18b78241de66e6d65284076b899539a31a082
ValeriiaMur <41483072+ValeriiaMur@users.noreply.github.com>
2020-10-31 21:56:10 -0700

    Update Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -7,18 +7,9 @@ function Start({stats, setStats,handleChanges}) {
 
     return (
         <div className = "start-page">
-        <h2>Do you think you know eveything? Test yourself with this trivia!</h2>
+        <h2>Do you think you know everything? Test yourself with this trivia!</h2>
         <p> You have only 3 minutes to answer all the questions</p>
         <img className="main-img" src={mainImage} alt="Start trivia"/>
-        {/* planned to but didn't have enough time to implement the player name and history */}
-        {/* <p>What's your name?</p>
-        <input 
-                type = "text"
-                id = "player"
-                value = {stats.player}
-                name="player"
-                onChange={handleChanges}>
-        </input> */}
         <Link to="/trivia" className="start-button">Start</Link>
         </div>
     );
